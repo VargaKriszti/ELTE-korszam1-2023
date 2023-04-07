caf0912b2d017e52572d5067d7e9392dce63baa6
VargaKriszti <128630358+VargaKriszti@users.noreply.github.com>
2023-04-07 18:37:05 +0200

    projekt

---
+++ newtonit/newtonit/newtonit.cpp
@@ -0,0 +1,12 @@
+﻿// newtonit.cpp : Defines the entry point for the application.
+//
+
+#include "newtonit.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
