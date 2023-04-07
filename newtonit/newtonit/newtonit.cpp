154f9ebf8c26ae84357b4e16346d089879efc6c3
VargaKriszti <128630358+VargaKriszti@users.noreply.github.com>
2023-04-07 17:09:38 +0000

    első beadandó

--- newtonit/newtonit/newtonit.cpp
+++ newtonit/newtonit/newtonit.cpp
@@ -1,12 +1,29 @@
-﻿// newtonit.cpp : Defines the entry point for the application.
-//
-
-#include "newtonit.h"
+﻿#include <iostream>
+#include <math.h>
 
 using namespace std;
 
+template <typename F, typename T, typename dF>
+T Newtonit(F f, dF df, T x0)
+{
+    T a = x0;
+    for (int i = 1; i < 100; i++) //csak 100 iteráció
+    {
+        T h = f(x0) / df(x0);
+        a = x0 - h;
+		x0 = a;
+    }
+    return a;
+}
+
 int main()
 {
-	cout << "Hello CMake." << endl;
-	return 0;
+    //megadott paraméterek
+    double x0 = 10.0;
+    auto f = [](double x) {return x * x - 612.0; };
+    auto df = [](double x) {return 2 * x; };
+
+    double eredmeny = Newtonit(f, df, x0);
+    std::cout << eredmeny << std:: endl;
+    return 0;
 }
